86dbd15ba07fbfacf0bf46edbb07e716c1a09993
Mani Bomma <Manikanta.Bomma@Teradata.com>
2018-07-17 23:57:42 +0530

    Serving static content using app.use()

--- MessagingApp/server.js
+++ MessagingApp/server.js
@@ -2,6 +2,7 @@ var express = require('express')
 
 var app = express()
 
+app.use(express.static(__dirname))
 var server = app.listen(3000, () => {
     console.log('Listening to port ', server.address().port)
 })
