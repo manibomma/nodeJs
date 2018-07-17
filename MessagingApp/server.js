e5e1812b977352cb65eb6bfc8df053a6cf8b6897
Mani Bomma <Manikanta.Bomma@Teradata.com>
2018-07-18 00:47:37 +0530

    created a get service that responds with hello

--- MessagingApp/server.js
+++ MessagingApp/server.js
@@ -3,6 +3,11 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+app.get('/messages', (req, res) => {
+    res.send('hello')
+})
+
 var server = app.listen(3000, () => {
     console.log('Listening to port ', server.address().port)
 })
