0f4b855485ca0b1853fc3ca9286b3af6bd897aee
Mani Bomma <Manikanta.Bomma@Teradata.com>
2018-07-19 21:45:38 +0530

    handling urlencoded browser data

--- MessagingApp/server.js
+++ MessagingApp/server.js
@@ -10,11 +10,14 @@ var messages = [
 
 app.use(express.static(__dirname))
 app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:false}))
+
 app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
 app.post('/messages', (req, res) => {
+    console.log(req.body)
     messages.push(req.body)
     res.sendStatus(200)
 })
