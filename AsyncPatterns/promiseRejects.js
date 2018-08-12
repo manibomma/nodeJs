f6c43d68c69f17eacb614de4990c0331f0784536
Mani Bomma <Manikanta.Bomma@Teradata.com>
2018-08-12 16:30:09 +0530

    custom promise rejection

--- AsyncPatterns/promiseRejects.js
+++ AsyncPatterns/promiseRejects.js
@@ -1,11 +1,13 @@
 var delay = (seconds) => new Promise((resolves, rejects) => {
-    throw new Error("Promise rejected")
+    if(seconds > 4) {
+        rejects(new Error(`Waiting ${seconds} seconds is too long`))
+    }
 
     setTimeout(()=> {
         resolves('Hey, your wait has come to an end')
     }, seconds * 1000)
 })
 
-delay(2)
+delay(10)
     .then((msg)=>{console.log(msg)})
     .catch((err)=> {console.log(`Error Occured : ${err.message}`)})
