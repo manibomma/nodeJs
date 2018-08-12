de86e6d6685f0ecea992210b995a2cf810ed4dbb
Mani Bomma <Manikanta.Bomma@Teradata.com>
2018-08-13 00:06:37 +0530

    Created sequential execution process of creating a file and unlinking the same file

---
+++ AsyncPatterns/sequentialExecution.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+
+var { promisify } = require('util')
+
+var writeFile = promisify(fs.writeFile)
+var unlinkFile = promisify(fs.unlink)
+
+var beep = () => process.stdout.write('\x07')
+
+var delay = (seconds) => new Promise((resolves, rejects) => {
+    setTimeout(resolves, seconds * 1000)
+})
+
+var createAndUnlinkFile = Promise.resolve()
+                            .then(() => console.log('Starting Operation..'))
+                            .then(() => console.log('waiting..'))
+                            .then(() => delay(2))
+                            .then(() => writeFile('newSample.txt', 'This is a file created from sequentialExecution.js'))
+                            .then(beep)
+                            .then(() => console.log('File created successfully'))
+                            .then(() => delay(10))
+                            .then(() => unlinkFile('newSample.txt'))
+                            .then(beep)
+                            .then(() => console.log('File unlinked successfully'))
+                            .catch((error) => console.log(`Error : ${error.message}`))
